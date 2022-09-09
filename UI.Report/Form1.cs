f9750a974dacc5a82d9fe8fcc299f84c21759fe4
Pasha <pashaie@gmail.com>
2022-09-09 12:28:15 +0430

    report

---
+++ UI.Report/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Stimulsoft.Report;
+
+namespace UI.Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            using (var db = new NorthWindEntities())
+            {
+                var products = db.Products.ToList();
+                var report = new StiReport();
+                report.RegBusinessObject("ProductBO", products);
+                report.Load(Path.Combine(Application.StartupPath, "ProductReport.mrt"));
+                report.Show();
+
+            }
+        }
+    }
+}
